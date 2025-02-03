adcfd630186e7c06978e5b504a33c25395454275
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2025-02-03 12:39:00 +0000

    (automated) deploy from commit 19475b23358a61bd6a25baa2cdb285a59ce46857

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["wiwi_wasm",[["impl&lt;S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wiwi_wasm/reflect/set/struct.Builder.html\" title=\"struct wiwi_wasm::reflect::set::Builder\">Builder</a>&lt;'_, S&gt;<div class=\"where\">where\n    S: <a class=\"trait\" href=\"wiwi_wasm/reflect/set/trait.State.html\" title=\"trait wiwi_wasm::reflect::set::State\">State</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[487]}
