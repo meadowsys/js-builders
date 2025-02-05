9d5085f482438bfc4f32e228fcc244f6bf4deadb
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2025-02-05 10:50:00 +0000

    (automated) deploy from commit 0c05ce8f36f07a87b239a93ac88c9f17d4fa1a36

--- src-files.js
+++ src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["wiwi_wasm",["",[["reflect",[],["prevent_extensions.rs","set.rs"]]],["any.rs","bigint.rs","boolean.rs","builder.rs","extern_crates.rs","lib.rs","number.rs","object.rs","reflect.rs","sealed.rs","string.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["wiwi_wasm",["",[["reflect",[],["apply.rs","construct.rs","define_property.rs","delete_property.rs","get.rs","get_own_property_descriptor.rs","get_prototype_of.rs","has.rs","is_extensible.rs","own_keys.rs","prevent_extensions.rs","set.rs","set_prototype_of.rs"]]],["any.rs","bigint.rs","boolean.rs","builder.rs","extern_crates.rs","lib.rs","number.rs","object.rs","reflect.rs","sealed.rs","string.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[208]}
+//{"start":36,"fragment_lengths":[404]}
